c4a3d31185d0161bb3ef8889de958d370ce130ec
nermin20 <nerminpesiyeva18@gmail.com>
2024-10-07 17:40:58 +0400

    new commit

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,10 +1,25 @@
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int teklik = a%10;
-        System.out.println(teklik);
+//import java.util.Scanner;
+//public class Main{
+//    public static void main(String[] args){
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int teklik = a%10;
+//        System.out.println(teklik);
+//
+//    }
+//}
 
-    }
-}
+//import java.util.Scanner;
+//public class Main{
+//    public static void main(String[] args){
+//        Scanner scanner = new Scanner(System.in);
+//        int a =scanner.nextInt();
+//        int evvelkicuteded;
+//        if(a%2==0){
+//            evvelkicuteded= a-2;
+//        }else{
+//            evvelkicuteded = a-1;
+//        }
+//        System.out.println(evvelkicuteded);
+//    }
+//}
